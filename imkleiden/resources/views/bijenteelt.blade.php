ffc4e34dbbca72d771c5098469a569e28abf2ad1
SanderGrimmelikhuizen <48210381+SanderGrimmelikhuizen@users.noreply.github.com>
2020-06-25 11:28:59 +0200

    Tijdige Bijenteelt pagina niet af
    Even om het te kunnen pushen naar feature2

---
+++ imkleiden/resources/views/bijenteelt.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Imkers Leiden Project">
+    <meta name="author" content="Sander Grimmelikhuizen, Eloy Maddamin, Marciano Hardoar">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Imkers Leiden</title>
+
+
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Imkers Leiden</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Activiteiten</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Bezittingen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Bijen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Bijenteelt</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Cursussen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Leden</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Bijennest melden</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Rooster</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Tips</a>
+                </li><li class="nav-item">
+                    <a class="nav-link disabled" href="#">Vereniging</a>
+                </li><li class="nav-item">
+                    <a class="nav-link disabled" href="#">Wespen</a>
+                </li><li class="nav-item">
+                    <a class="nav-link disabled" href="#">Zwerm</a>
+            </ul>
+            <button type="button" class="btn btn-success navbar-btn btn-circle">Admin-panel</button>
+        </div>
+    </nav>
+</header>
+
+{{--Jumbotron--}}
+<div class="jumbotron">
+    <center><h1 class="display-4">Over onze bijenteelt!</h1>
+        <p class="lead">Bijenteelt wordt door ons beheert in Leiden</p>
+        <hr class="my-4">
+        <p>Wij zijn experts op het gebied van bijenteelt en zwermen.</p>
+        <p class="lead">
+            <a class="btn btn-success btn-lg" href="#" role="button">Meer over onze bijenteelt!</a>
+        </p></center>
+</div>
+
+
+
+
+<main role="main">
+    <center><h2>Onze top bijenteelt kanidaten</h2></center>
+    <table class="table table-borderless">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Naam</th>
+            <th>Achternaam</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Bert</td>
+            <td>Ippaloy</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jason</td>
+            <td>De Hront</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Mick</td>
+            <td>De Hront</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jason</td>
+            <td>De Hront</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jason</td>
+            <td>De Hront</td>
+        </tr>
+        </tbody>
+    </table>
+
+
+
+    </div><!-- /.container -->
+
+
+    <!-- FOOTER -->
+    <footer class="container">
+        <p class="float-right"><a href="#">Terug naar boven</a></p>
+        <p>© 2020 imk leiden Project Groep 3 · <a href="#">Privacy</a> · <a href="#">Condities</a></p>
+    </footer>
+</main>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg></body>
+</html>
